c063bde5f83af9df50dc392252ce0b598e6bf9a1
khantminht <khantminht.bizs@gmail.com>
2022-07-05 20:01:09 +0630

    05july

---
+++ js/app.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+	// console.log("hi");
+
+	// Start nav
+
+	$(".navbuttons").click(function(){
+		$(".navbuttons").toggleClass("crossxs");
+	});
+
+	// End nav
+
+	//fornav
+	$(window).scroll(function(){
+
+		let getscrolltop = $(this).scrollTop();
+		// console.log(getscrolltop);
+
+		if(getscrolltop >= 200){
+			$(".navbar").addClass("navmenus");
+		}else{
+			$(".navbar").removeClass("navmenus");
+		}
+
+	});
+
+	// Start Properties
+
+	$(".propertylists").click(function(){
+
+		$(this).addClass("activeitems").siblings().removeClass("activeitems");
+	
+
+		// for filter
+
+		let getattvalue = $(this).attr("data-filter");
+		// console.log(getattvalue);
+
+		if(getattvalue === "all"){
+			$(".filters").show("slide",500);
+
+		}else{
+
+			$(".filters").hide();
+			$(".filters").not("."+ getattvalue).hide("slide",500);
+			$(".filters").filter("."+ getattvalue).show("slide",500);
+		}
+
+
+	});
+
+	// For image overlay and show
+	lightbox.option({
+		showImageNumberLabel:false
+	});
+
+
+	// End Properties
+
+
+	// Start Adv Section
+
+	$(window).scroll(function(){
+		let getscrolltop = $(this).scrollTop();
+		// console.log(getscrolltop);
+
+		if(getscrolltop >= 900){
+			$(".advimages").addClass("fromlefts");
+			$('.advtexts').addClass("fromrights");
+		}else{
+			$(".advimages").removeClass("fromlefts");
+			$(".advtexts").removeClass("fromrights");
+		}
+	})
+
+	// End Adv Section
+
+});
